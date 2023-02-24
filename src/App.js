a935955c763b542a2b80a3811049774f6e6ba9cf
Linda Keating <lindakeating@lindas-mbp.home>
2023-02-24 11:36:47 +0000

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { connect } from "react-redux";
+import MyCrossword from './Crossword';
+import { ImageContainer } from './components/Image';
+import { CurrentClueContainer } from './components/CurrentClue';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: '100%' }}>
+        <div>
+        </div>
+        <CurrentClueContainer />
+        <MyCrossword />
+      </div>;
     </div>
   );
 }
